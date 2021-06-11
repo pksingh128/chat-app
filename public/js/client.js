280f014c6667ec39e316707b1044cac7d58d65d0
prashant <pksingh706586@gmail.com>
2021-06-12 02:12:23 +0530

    for deploy

---
+++ public/js/client.js
@@ -0,0 +1,72 @@
+const socket = io();
+//get DOM elements in respective js variables
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+//audio that will play on recieving the messeges
+var audio = new Audio('./ting.mp3');
+
+
+//function that will append event info to the container
+const append = (message, postion) => {
+  const messageElement = document.createElement("div");
+  //messageElement.innerText  = time;
+ 
+
+  messageElement.classList.add("message");
+  messageElement.classList.add(postion);
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+  scrollToBottom()
+  if (postion == "left") {
+    audio.play();
+    
+  }
+};
+
+const { name } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+console.log(name);
+// let name;
+// do{
+//    name = prompt("Enter your name to join");
+// }while(!name)
+
+//ask new user for his/her name and let the server know
+socket.emit("new-user-joined", {name});
+
+
+
+
+//if a new user joins,receive the name event from the server
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "right");
+  scrollToBottom();
+});
+
+//if server sends the messages ,receive it
+socket.on("receive", (data) => {
+  append(`${data.name}: ${data.message}`, "left");
+});
+
+//if user leaves the chats ,append the info to the container
+socket.on("left", (name) => {
+  append(`${name} left the chat`,  "right");
+});
+
+//if the forms get submitted, send serverthe message
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, "right");
+    socket.emit("send", message);
+    //clear the message 
+    messageInput.value = "";
+  });
+
+
+  //scroll down
+  function scrollToBottom(){
+    messageContainer.scrollTop = messageContainer.scrollHeight
+  }
---
+++ server.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').createServer(app);
+const moment = require('moment');
+
+
+const PORT = process.env.PORT || 8080
+http.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
+})
+
+//middleware to render public file
+app.use(express.static(path.join(__dirname, 'public')));
+
+const users = {};
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+//socket
+const io = require('socket.io')(http);
+
+io.on('connection', (socket)=>{
+    console.log("connected..")
+    //if any new user joins, let other users connected to the server know.
+    socket.on('new-user-joined',({name}) =>{
+        //console.log("new user",name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    });
+
+    
+    //if someone sends the message, broadcast to other people
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]});
+    });
+
+
+//if someone leave the chat let other know
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
